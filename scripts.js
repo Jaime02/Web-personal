58c40223c33d3419be886e9b7ee7456b4941ee2f
Jaime Resano <gemailpersonal02@gmail.com>
2020-04-18 12:40:35 +0200

    Añadido modo claro Mejorado el ratio de aspecto de los vídeos

--- scripts.js
+++ scripts.js
@@ -4,8 +4,8 @@ wordWrapperContent = wordWrapper.innerHTML,
 addingWord = false,
 counter = 1;
 
+// Esta función hace el efecto de borrar el texto
 setInterval(function(){
-
   if(wordWrapperContent.length > 0 && !addingWord ) {
     wordWrapper.innerHTML = wordWrapperContent.slice(0, -1);
     wordWrapperContent = wordWrapper.innerHTML;
@@ -13,20 +13,40 @@ setInterval(function(){
     addingWord = true;
   }
 
-  if( addingWord ){
-    if( wordWrapperContent.length < words[counter].length  ) {
+  if(addingWord){
+    if(wordWrapperContent.length < words[counter].length) {
       wordWrapper.innerHTML = words[counter].slice(0, wordWrapperContent.length + 1);
       wordWrapperContent = wordWrapper.innerHTML;
     } else {
-      if( counter < words.length) {
+      if (counter < words.length) {
         counter ++
       }
       addingWord = false;
     }
   }
-
-  if( counter == words.length) {
+  if (counter == words.length) {
     counter = 0;
   }
 
 }, 250);
+
+// Función para activar el modo oscuro
+var modo_oscuro = true;
+function cambiar() {
+  var img1 = "./Bombilla.png";
+  var img2 = "./Luna.png";
+  var element = document.body;
+
+  var imgElement = document.getElementById('imagen'); 
+
+  if (modo_oscuro) {
+    imgElement.src = img2;
+    modo_oscuro = false;
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+  else {
+    imgElement.src = img1;
+    modo_oscuro = true;
+    document.documentElement.setAttribute('data-theme', 'dark');
+  }
+}
