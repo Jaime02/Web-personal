c9d8639c0ff043fd8bd530852236d4ac2c23bdfc
Jaime Resano <gemailpersonal02@gmail.com>
2020-05-07 13:09:26 +0200

    Añadido banner

--- scripts.js
+++ scripts.js
@@ -1,66 +1,72 @@
-var words = ['Python.', 'la programación.', 'OpenGL.', 'Sympy.', 'PyQt5.', 'aprender.','las matemáticas.', 'el código abierto.', 'los grandes proyectos.', 'nuevas tecnologías.'],
-wordWrapper = document.getElementById('word');
+var words = ['Python.', 'la programación.', 'OpenGL.', 'Sympy.', 'PyQt5.', 'aprender.', 'las matemáticas.', 'el código abierto.', 'los grandes proyectos.', 'nuevas tecnologías.'];
+var wordWrapper = document.getElementById('word');
 wordWrapperContent = wordWrapper.innerHTML;
 addingWord = false;
 counter = 0;
 esperar = false;
 
-// Esta función hace el efecto de borrar el texto
 function loop() {
-setTimeout(async ()=>{
-  if (esperar){
-    esperar = false;
-    await new Promise(r => setTimeout(r, 2000));
-  }
+  // Esta función hace el efecto de borrar el texto
+  setTimeout(async () => {
+    if (esperar) {
+      esperar = false;
+      await new Promise((r) => setTimeout(r, 2000));
+    }
 
-  if (wordWrapperContent.length > 0 && !addingWord) {
-    wordWrapper.innerHTML = wordWrapperContent.slice(0, -1);
-    wordWrapperContent = wordWrapper.innerHTML;
-  } else {
-    addingWord = true;
-  }
-
-  if (addingWord){
-    if(wordWrapperContent.length < words[counter].length) {
-      wordWrapper.innerHTML = words[counter].slice(0, wordWrapperContent.length + 1);
+    if (wordWrapperContent.length > 0 && !addingWord) {
+      wordWrapper.innerHTML = wordWrapperContent.slice(0, -1);
       wordWrapperContent = wordWrapper.innerHTML;
     } else {
-      if (counter < words.length) {
-        esperar = true;
-        counter++;
-      }
-      addingWord = false;
+      addingWord = true;
     }
-  }
 
-  if (counter == words.length) {
-    counter = 0;
-  }
+    if (addingWord) {
+      if (wordWrapperContent.length < words[counter].length) {
+        wordWrapper.innerHTML = words[counter].slice(0, wordWrapperContent.length + 1);
+        wordWrapperContent = wordWrapper.innerHTML;
+      } else {
+        if (counter < words.length) {
+          esperar = true;
+          counter++;
+        }
+        addingWord = false;
+      }
+    }
 
-  loop();
-}, Math.floor(Math.random() * 250 + 80));
+    if (counter == words.length) {
+      counter = 0;
+    }
+
+    loop();
+  }, Math.floor(Math.random() * 250 + 50));
 } loop();
 
 
 // Función para activar el modo oscuro
 var modo_oscuro = true;
 function cambiar() {
-  var img1 = "./Bombilla.png";
-  var img2 = "./Luna.png";
-  var element = document.body;
+  const img1 = './Bombilla.png';
+  const img2 = './Luna.png';
+  const banner_claro = './Banner.png';
+  const banner_oscuro = './Banner oscuro.png';
+  const element = document.body;
 
-  var imgElement = document.getElementById('imagen'); 
+  const imgElement = document.getElementById('imagen');
+  const banner = document.getElementById('Banner');
 
   if (modo_oscuro) {
     imgElement.src = img2;
+    banner.src = banner_claro;
     modo_oscuro = false;
     document.documentElement.setAttribute('data-theme', 'light');
-  }
-  else {
+  } else {
     imgElement.src = img1;
+    banner.src = banner_oscuro;
     modo_oscuro = true;
     document.documentElement.setAttribute('data-theme', 'dark');
   }
 }
 
-document.querySelector("#imagen").addEventListener('click',()=>{cambiar();});
+document.querySelector('#imagen').addEventListener('click', () => {
+  cambiar();
+});
