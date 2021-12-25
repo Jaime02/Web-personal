d5639eda88eb43e79fae63b7d19663d7979f69aa
Jaime Resano <gemailpersonal02@gmail.com>
2021-12-25 13:19:35 +0100

    Add lazy load. Experimental

--- scripts.js
+++ scripts.js
@@ -41,25 +41,21 @@ function loop() {
   }, Math.floor(Math.random() * 250 + 50));
 } loop();
 
-
 // Toggle dark mode
 var dark_mode = true;
-function cambiar() {
-  const img1 = './Bombilla.png';
-  const img2 = './Luna.png';
+function toggleTheme() {
   const imgElement = document.getElementById('imagen');
-
   if (dark_mode) {
-    imgElement.src = img2;
+    imgElement.src = './dark-mode.png';
     dark_mode = false;
     document.documentElement.setAttribute('data-theme', 'light');
   } else {
-    imgElement.src = img1;
+    imgElement.src = './light-mode.png';
     dark_mode = true;
     document.documentElement.setAttribute('data-theme', 'dark');
   }
 }
 
 document.querySelector('#imagen').addEventListener('click', () => {
-  cambiar();
+  toggleTheme();
 });
